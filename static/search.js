12e16861ceb1fa1b2657e8ed0d55799d00bff625
Daniel Naval <danielnaval93@gmail.com>
2014-06-04 17:49:16 +0000

    Modified search() to use jsonp requesting due to cross-domain issues. Also added a small block that will prevent the form from refreshing the page. Added test.html, which is just a dummy html file for testing purposes.

--- static/search.js
+++ static/search.js
@@ -1,18 +1,34 @@
-function search() {
-    var script = document.createElement('script');
-    script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(script);
-    var productName = $("#productName").value;
-    var requestURL = "https://api.sgt.io/products/?api_key=4c4ec8961617eee94e2469b1fea9f5587d64b6d3&q=" + productName;
+// When the page loads, we will set the form's submit function to searchAPI.
+// Returning false prevents page refresh
+$(document).ready(function() {
+    $("form").submit(function (e) {
+        e.preventDefault();
+        searchAPI();
+    });
+})
+
+function searchAPI() {
+    var productName = document.getElementById("productName").value;
+    var requestURL = "https://api.sgt.io/products/?api_key=14a5eb7c31cdf6e9a9758ba7f32224c20fcc1137&q=" + productName + "&format=jsonp";
+    // Cross-domain issues came up, so we're using this niftier ajax request
     $.ajax({
         url: requestURL,
-        type: 'GET',
-        success: display,
-        error: searchError
+        type: "GET",
+        crossDomain: true,
+        dataType: "jsonp",
+        jsonpCallback: "display",
+        timeout: 1000
     });
 }
 
 function searchError() {
     // Probably should do an error log here
+    document.getElementById("textBox").innerHTML = "Failed";
+    return false;
+}
+
+// array is an array of Objects that represent the returned json objects from the api
+function display(array) {
+    document.getElementById("textBox").innerHTML = "Success";
+    return false;
 }
