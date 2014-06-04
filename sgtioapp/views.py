fe8081de105c97f834e070d3fb897ede6d53a678
Jason Li <jason.w.li112@gmail.com>
2014-06-04 20:21:45 +0000

    working skeleton of introproject

--- sgtioapp/views.py
+++ sgtioapp/views.py
@@ -7,15 +7,12 @@ from django.shortcuts import render_to_response
 import requests
 # Create your views here.
 
-def sgtio_new(request):
-	return
-
 def sgtio_template(request):
         product = request.GET.get("search")
 
         url = "https://api.sgt.io/products/?api_key=4c4ec8961617eee94e2469b1fea9f5587d64b6d3&q=" + product + "&format=json"
 
         r = requests.get(url)
-
-	return render_to_response('template.html', )
+        products = {"products":r.json()}
+	return render_to_response('template.html',products)
 
