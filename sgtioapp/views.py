e9bea134efb3ee32754ec63b11f11fac704ca6c9
Jason Li <jason.w.li112@gmail.com>
2014-06-04 21:07:28 +0000

    added logging functionality:

--- sgtioapp/views.py
+++ sgtioapp/views.py
@@ -1,15 +1,12 @@
-from django.conf import settings
-from django.shortcuts import render
 from django.core.management.base import NoArgsCommand
-from django.template import Context, Template
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import requests
-# Create your views here.
-
+import logging
+logger = logging.getLogger(__name__)
 def sgtio_template(request):
-        product = request.GET.get("search")
 
+        product = request.GET.get("search")
+        logger.debug("User searched for " + product)
         url = "https://api.sgt.io/products/?api_key=4c4ec8961617eee94e2469b1fea9f5587d64b6d3&q=" + product + "&format=json"
 
         r = requests.get(url)
