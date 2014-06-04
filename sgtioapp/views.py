fd32a38c6cc2f623190c40830b4e4e1ad772a3fc
Jason Li <jason.w.li112@gmail.com>
2014-06-04 20:08:36 +0000

    modified templates and views and added requirements

--- sgtioapp/views.py
+++ sgtioapp/views.py
@@ -4,42 +4,18 @@ from django.core.management.base import NoArgsCommand
 from django.template import Context, Template
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+import requests
 # Create your views here.
 
-def get_json_object(json):
-	return json
+def sgtio_new(request):
+	return
 
 def sgtio_template(request):
-	
-	
-	c = {"products":
-			[{
-        		"title": "Apple iPhone 3G - 8GB - Black (Unlocked) Smartphone New", 
-        		"price": "119.95", 
-        		"description": "fresca", 
-        		"merchant": "eBay", 
-        		"url": "http://api.socialingot.com/click/widget/?afsrc=1&key=208277&deal=467287&url=http://www.ebay.com/itm/Apple-iPhone-3G-8GB-Black-Unlocked-Smartphone-New-/161280045139?pt=Cell_Phones", 
-        		"shipping_cost": "0.00", 
-        		"currency_code": "USD", 
-        		"image_url": "http://thumbs4.ebaystatic.com/m/mhRaAtxUwQ_ttPUq3Zac3qg/140.jpg"
-   		},
-		{
-			"title": "? Apple iPHONE 3GS - 8GB BLACK - (Factory Unlocked) SmartPhone Cell Phone", 
-        		"price": "119.95", 
-        		"description": "no descrip", 
-        		"merchant": "eBay", 
-        		"url": "http://api.socialingot.com/click/widget/?afsrc=1&key=208277&deal=467287&url=http://www.ebay.com/itm/Apple-iPHONE-3GS-8GB-BLACK-Factory-Unlocked-SmartPhone-Cell-Phone-/151317976431?pt=Cell_Phones", 
-        		"shipping_cost": "0.00", 
-        		"currency_code": "USD", 
-        		"image_url": "http://thumbs4.ebaystatic.com/m/mAg_WtIkLw4PMAQGdMV5MrA/140.jpg"
-			}]
-		}
-		
+        product = request.GET.get("search")
 
-	
-	#f = open('write_test.txt', 'w')
-	#f.write(t.render(c))
-	#f.close
+        url = "https://api.sgt.io/products/?api_key=4c4ec8961617eee94e2469b1fea9f5587d64b6d3&q=" + product + "&format=json"
 
-	return render_to_response('template.html', c)
+        r = requests.get(url)
+
+	return render_to_response('template.html', )
 
