0312b9599c6736b6be2f9586e464f0b4b3bfbbc0
Jason Li <jason.w.li112@gmail.com>
2014-06-05 13:30:45 +0000

    fixed a bug with the views (error when user didn't enter anything)

--- introproject/settings.py
+++ introproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sgtioapp',
+    'bootstrap_toolkit',
 )
 
 MIDDLEWARE_CLASSES = (
--- sgtioapp/views.py
+++ sgtioapp/views.py
@@ -6,7 +6,8 @@ logger = logging.getLogger(__name__)
 def sgtio_template(request):
 
         product = request.GET.get("search")
-        logger.debug("User searched for " + product)
+        if product:
+            logger.debug("User searched for " + product)
         url = "https://api.sgt.io/products/?api_key=4c4ec8961617eee94e2469b1fea9f5587d64b6d3&q=" + product + "&format=json"
 
         r = requests.get(url)
