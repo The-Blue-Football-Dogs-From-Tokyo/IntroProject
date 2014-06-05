db6cc4fbb005c8d99b42886cf1160e90e936e1c6
Jason Li <jason.w.li112@gmail.com>
2014-06-05 19:44:43 +0000

    set up xcoverage and added 2 tests for webpage

--- sgtioapp/tests.py
+++ sgtioapp/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
 from sgtioapp import views
+from django.test.client import Client
+import requests
 # Create your tests here.
 
 class ViewTestCase(TestCase):
-    def test_connection(self):
-        self.assertEquals(1+1, 2)
-    #def test_null():
+    def setUp(self):
+        self.client = Client()
 
-    #def test_
+    def test_connection(self):
+        response = self.client.get('/sgtioapp/')
+        self.assertEquals(200, response.status_code)
+
+    def test_search(self):
+        response = self.client.get('/sgtioapp/?search=iphone')
+        self.assertEquals(200, response.status_code)
+        self.assertNotEquals(response.content, None)
